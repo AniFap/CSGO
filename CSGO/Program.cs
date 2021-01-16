fbffa95710f81f0d3b7caf8d58daed65ae904dfa
Maxim Goryachev <salem1337@yandex.ru>
2021-01-16 18:57:14 +0300

    Revert "Добавил вечную функцию"
    This reverts commit dfaaedf9a8a7e809e0431423a8e99dcf9583024c.

--- CSGO/Program.cs
+++ CSGO/Program.cs
@@ -6,10 +6,6 @@ namespace CSGO
     {
         static void Main()
         {
-            while (true)
-            {
-                Console.WriteLine("Супер пупер чит");
-            }
         }
     }
 }
